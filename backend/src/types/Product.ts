99e8d9e2ccf1ec0a7d39532eeb87e728eb465170
Ethan Jiang <jyx88056601@gmail.com>
2023-11-06 19:39:24 -0800

    Initialize backend

---
+++ backend/src/types/Product.ts
@@ -0,0 +1,14 @@
+export type Product = {
+    name : string;
+    slug : string;
+    image : string;
+    category : string;
+    brand : string;
+    description : string;
+
+    price : number;
+    countInStock : number;
+   
+    numReviews : number;
+    rating : number;
+}
